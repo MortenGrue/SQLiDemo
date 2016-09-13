128a2f4728bfccf8b108e4daccc9c7a6f04e0c8f
MortenGrue <MortenGrue@Gmail.com>
2016-09-13 19:43:42 +0200

    CleanUp

--- SQLiDemoUserAgent/AttributeFilters/LogUserAgentAttribute.cs
+++ SQLiDemoUserAgent/AttributeFilters/LogUserAgentAttribute.cs
@@ -19,30 +19,23 @@ namespace SQLiDemoUserAgent.AttributeFilters
                 {
                     con.Open();
 
-                    string sql = "SELECT 1 FROM dbo.UserAgentLog WHERE UserAgent = '" + filterContext.HttpContext.Request.Headers["User-Agent"] + "' AND Page = 'Blog';";
+                    string sql = "SELECT UserAgent, Page FROM dbo.UserAgentLog WHERE Page = 'Blog' AND UserAgent = '" + filterContext.HttpContext.Request.Headers["User-Agent"] + "' ;";
                     SqlCommand comm = new SqlCommand(sql, con);
-
                     var res = comm.ExecuteScalar();
 
                     if (res != null)
-                    {
                         sql = "UPDATE dbo.UserAgentLog SET Count = Count + 1 WHERE UserAgent = @UserAgent AND Page = 'Blog';";
-                    }
                     else
-                    {
                         sql = "INSERT INTO dbo.UserAgentLog (UserAgent, Page) VALUES (@UserAgent, 'Blog');";
-                    }
 
                     SqlCommand comm2 = new SqlCommand(sql, con);
                     comm2.Parameters.AddWithValue("@UserAgent", filterContext.HttpContext.Request.Headers["User-Agent"]);
-
                     comm2.ExecuteNonQuery();
                 }
             }
             catch
             {
-                //All done, took 10min to program!
-                //We dont care that it failed - UseLess data anyway :)
+                //All done, took 10min to program! - We dont care that it failed - UseLess data anyway :)
             }
             base.OnActionExecuting(filterContext);
         }
