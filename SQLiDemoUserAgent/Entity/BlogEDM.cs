c725fa370a358297caf3f6ebce865904fb46128d
MortenGrue <MortenGrue@Gmail.com>
2016-09-13 23:28:02 +0200

    Removed Usings

--- SQLiDemoUserAgent/Entity/BlogEDM.cs
+++ SQLiDemoUserAgent/Entity/BlogEDM.cs
@@ -1,9 +1,6 @@
 namespace SQLiDemoUserAgent.Entity
 {
-    using System;
     using System.Data.Entity;
-    using System.ComponentModel.DataAnnotations.Schema;
-    using System.Linq;
 
     public partial class BlogEDM : DbContext
     {
